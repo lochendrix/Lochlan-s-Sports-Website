e007d1e3aea985033d025840a814f3973c1e635c
lochendrix <97645511+lochendrix@users.noreply.github.com>
2022-11-01 01:18:48 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,12 @@ export default function Home() {
         <p className="description">
           Last updated: 
         </p>
+    
+    <object data="/path_to_text_file/text.txt" type="text/plain"
+width="500" style="height: 300px">
+<a href="/Users/lochlanhendrix/Desktop/Scores.txt">No Support?</a>
+</object>
+    
       </main>
 
       <Footer />
